322f0804a7816b177ff60530d51f0722463fbfd2
sbagos27 <sbagos@csumb.edu>
2025-02-04 18:56:23 -0800

    Got sound to work on trampolines

--- Assets/Nuthin but Donut/Scripts/Trampoline.cs
+++ Assets/Nuthin but Donut/Scripts/Trampoline.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     public float impulseStrength = 1f;
+    public AudioClip boingClip;
 
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log($"{this.name} collided with the {collision.gameObject.name}");
         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
         rb.AddForce(transform.up * impulseStrength, ForceMode.Impulse);
+        
+        AudioSource audioSrc = GetComponent<AudioSource>();
+        audioSrc.clip = boingClip;
+        audioSrc.Play();
     }
 }
